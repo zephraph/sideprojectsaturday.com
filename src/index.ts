af671e1ab05d5b56df6d3f4b5327f389e51d58bf
Justin Bennett <git@just-be.dev>
2025-03-29 00:03:02 -0400

    Add .wrangler to gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import { createRouter } from "@actor-core/cloudflare-workers";
 import { setup } from "actor-core";
 import { Hono } from "hono";
+import { jsxRenderer } from "hono/jsx-renderer";
 import { doorActor } from "./actors/door.ts";
 import { eventActor } from "./actors/event.ts";
 
+import { Background } from "./components/Background.tsx";
 
 const app = new Hono();
+
+app.use(jsxRenderer());
 
 //#region Routes
 app.get("/", (c) => c.text("Welcome to my app!"));
@@ -13,11 +17,11 @@ app.get("/hello", (c) => c.text("Hello, world!"));
 //#endregion
 
 //#region ActorCore
-const ACTOR_ROUTE = "/actors/"
+const ACTOR_ROUTE = "/actors/";
 
 export const actorCore = setup({
 	actors: { door: doorActor, event: eventActor },
-	basePath: ACTOR_ROUTE
+	basePath: ACTOR_ROUTE,
 });
 
 const { router: actorRouter, ActorHandler } = createRouter(actorCore);
