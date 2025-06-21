12955564928f769b3169dcc4b925b5fea58c32e7
Justin Bennett <git@just-be.dev>
2025-06-20 23:07:57 -0400

    Add alchemy; get site deploying

---
+++ alchemy.run.ts
@@ -0,0 +1,14 @@
+import alchemy from "alchemy";
+import { Astro } from "alchemy/cloudflare";
+
+const app = await alchemy("sideprojectsaturday");
+
+export const worker = await Astro("sideprojectsaturday", {
+  command: "astro build",
+});
+
+console.log({
+  url: worker.url,
+});
+
+await app.finalize();
