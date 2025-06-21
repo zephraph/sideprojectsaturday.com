07734c7f272be47d8cb3ae45ea0be5dd7fb0652a
Justin Bennett <git@just-be.dev>
2025-06-21 11:13:38 -0400

    Add tailwind; daisyui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 
 import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	adapter: cloudflare(),
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
